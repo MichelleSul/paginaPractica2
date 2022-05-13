8c1192e7424cc7c93e37a4b904feb388451421ed
MichelleSul <102839909+MichelleSul@users.noreply.github.com>
2022-05-13 14:57:01 -0500

    Add files via upload

---
+++ Enviar.php
@@ -0,0 +1,29 @@
+<?php
+
+ $Destino ="mail@dominio.com";
+$Producto =$_POST('Producto');
+$Importe  =$_POST('Importe');
+$Talla   = $_POST('Talla');
+$Nombre   =$_POST('Nombre');
+$Telefono =$_POST('Telefono');
+$Correo  =$_POST('Correo');
+$Direccion =$_POST('Direccion');
+$CodigoPostal =$_POST('Codigo_Postal');
+
+
+$Contenido="Producto: " . $Producto . "\nImporte: " .$Importe . "\nTalla: " . $Talla .  "\nNombre: ". $Nombre . "\nTelefono: ".
+	$Telefono . "\nCorreo: " . $Correo . "\nDireccion: ". $Direccion . "\nCodigo Postal: " . $CodigoPostal;
+
+
+          mail(Destino,"PEDIDO: ",$Contenido);
+
+          header("location:Gracias.html");
+	
+	
+	
+	
+	
+	
+	
+	
+?>
